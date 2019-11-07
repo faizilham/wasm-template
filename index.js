cb2cf283c67392b0394d7097caab545ccdc293a5
Faiz Ilham <faizilham.faizz@gmail.com>
2019-11-07 20:49:14 +0700

    setup webpack

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import * as wasm from "./pkg";
+// import styles here
+import "./style/main.css"
+import "./style/other.css"
 
-wasm.greet();
+// A dependency graph that contains any wasm must all be imported
+// asynchronously. This file does the single async import, so
+// that no one else needs to worry about it again.
+import("./module.js")
+  .catch(e => console.error("Error importing module:", e));
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,46 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const production = process.env.NODE_ENV == "production";
+
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: production ? "index-[hash:8].js" : "index.js",
   },
-  mode: "production",
+  mode: production ? "production" : "development",
+
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin({}),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCSSExtractPlugin({
+      filename: production ? "style-[hash:8].css" : "style.css"
+    })
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          MiniCSSExtractPlugin.loader,
+          "css-loader"
+        ]
+      }
+    ]
+  }
 };
